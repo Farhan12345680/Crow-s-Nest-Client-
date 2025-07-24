7adae0eccfaa5ea1253905cf38d2e13431ba928c
Farhanadib577 <farhanadib577@gmail.com>
2025-07-24 22:59:10 +0600

    change1

--- src/main/java/com/example/crows__nest__client_trial/HelloApplication.java
+++ src/main/java/com/example/crows__nest__client_trial/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world12");
         launch();
     }
 }
